b95f79f75b48baff0d7a4de2748cfef28540ff82
T-Pose Ratkechi <tposeratkechi@gmail.com>
2021-07-13 14:19:26 -0500

    Created new project since renaming projects is... difficult.

---
+++ PhosMusicConverterCMD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhosMusicConverterCMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
